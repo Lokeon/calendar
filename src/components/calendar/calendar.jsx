0904ed25966096c4690d670eba0ca679abebbdba
Rodrigo Sanabria <rodrigosanabria22@gmail.com>
2019-02-06 16:38:05 +0100

    added bulma

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,4 +1,13 @@
 import React, { Component } from "react";
-
+import "./../style.scss";
 export default class Calendar extends Component {
+  render() {
+    return (
+      <section className="hero is-link">
+        <div className="hero-body">
+          <div className="columns">{this.props.children}</div>
+        </div>
+      </section>
+    );
+  }
 }
